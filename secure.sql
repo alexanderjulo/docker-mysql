47e12d37522286ab166c25e25b69956cbdd86838
Alexander Jung-Loddenkemper <alexander@julo.ch>
2014-04-30 10:38:07 +0200

    allow remote access.

--- secure.sql
+++ secure.sql
@@ -2,4 +2,5 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 UPDATE mysql.user SET Password=PASSWORD('insecure') WHERE User='root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
