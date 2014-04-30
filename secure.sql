aca94bddd758a8b8f6a740e6db390813c51f9e78
Alexander Jung-Loddenkemper <alexander@julo.ch>
2014-04-30 11:15:33 +0200

    fix remote access.

--- secure.sql
+++ secure.sql
@@ -2,5 +2,6 @@ DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 UPDATE mysql.user SET Password=PASSWORD('insecure') WHERE User='root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
+CREATE USER 'admin'@'%' IDENTIFIED BY 'insecure';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
