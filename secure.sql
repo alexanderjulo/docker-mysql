1521bf4c99ebbb12483b71e4befc5bd4b5a8dd78
Alexander Jung-Loddenkemper <alexander@julo.ch>
2014-04-30 11:23:12 +0200

    again.

--- secure.sql
+++ secure.sql
@@ -1,7 +1,14 @@
+# disable configure some stuff for security
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 UPDATE mysql.user SET Password=PASSWORD('insecure') WHERE User='root';
+
+# now let's make it worse than it was in the beginning and enable remote access
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'insecure';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 CREATE USER 'admin'@'%' IDENTIFIED BY 'insecure';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+
+# flush that shit
 FLUSH PRIVILEGES;
